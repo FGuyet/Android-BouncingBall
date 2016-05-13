1e181ae814a52f42bcb1b935dc5e168249cae1e5
FGuyet <florian.guyet@etu.utc.fr>
2016-05-14 00:58:58 +0200

    setting up SurfaceView : GameView

--- app/src/main/java/com/example/florian/android_bouncingball/GameView.java
+++ app/src/main/java/com/example/florian/android_bouncingball/GameView.java
@@ -1,24 +1,45 @@
 package com.example.florian.android_bouncingball;
 
 import android.content.Context;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
  * Created by Florian on 14/05/2016.
  */
 public class GameView extends SurfaceView implements Runnable{
+    Thread t = null;
+    SurfaceHolder holder;
+    boolean status = false;
 
     public GameView(Context context) {
         super(context);
+
+        holder = getHolder();
     }
 
     public void run(){
-
+        if (status){
+            //draw
+        }
     }
+
     public void pause(){
-
+        status = false;
+        while(true){
+            try{
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            break;
+        }
+        t = null;
     }
-    public void resume(){
 
+    public void resume(){
+        status = true;
+        t = new Thread(this);
+        t.start();
     }
 }
