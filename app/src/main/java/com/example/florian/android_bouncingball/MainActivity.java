ff86dbc8045c07b9988d0b290f034ffe72060711
FGuyet <florian.guyet@etu.utc.fr>
2016-05-14 02:07:01 +0200

    mouvement de la balle

--- app/src/main/java/com/example/florian/android_bouncingball/MainActivity.java
+++ app/src/main/java/com/example/florian/android_bouncingball/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.florian.android_bouncingball;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnTouchListener{
     GameView v;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +18,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         v = new GameView(this);
+        v.setOnTouchListener(this);
+
         setContentView(v);
     }
 
@@ -27,4 +35,8 @@ public class MainActivity extends Activity {
         v.resume();
     }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
 }
